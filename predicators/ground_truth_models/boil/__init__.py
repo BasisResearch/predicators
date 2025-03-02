5b48f6a6acbb68a5653aa1958e5a59e2e688f4f2
yichao-liang <ycliang6@gmail.com>
2025-03-02 16:37:08 +0000

    init boil model

---
+++ predicators/ground_truth_models/boil/__init__.py
@@ -0,0 +1,9 @@
+"""Ground-truth models for coffee environment and variants."""
+
+# from .nsrts import PyBulletBoilGroundTruthNSRTFactory
+from .options import PyBulletBoilGroundTruthOptionFactory
+
+__all__ = [
+    # "PyBulletBoilGroundTruthNSRTFactory", 
+    "PyBulletBoilGroundTruthOptionFactory"
+]
