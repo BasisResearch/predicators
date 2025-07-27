6bf323dc6c17740e1258abf72a9d72a093a02996
yichao-liang <yichao-liang>
2025-07-27 10:54:59 -0400

    Fix type check for predicates in task_plan_from_task function

--- predicators/planning_with_processes.py
+++ predicators/planning_with_processes.py
@@ -414,7 +414,7 @@ def task_plan_from_task(
 ) -> Iterator[Tuple[List[_GroundEndogenousProcess], List[Set[GroundAtom]],
                     Metrics]]:
     # TODO: Expand the concept predicates to include all dependencies
-    if type(predicates) is FrozenSet:
+    if isinstance(predicates, FrozenSet):
         predicates = set(predicates)
     assert isinstance(predicates, set), \
         f"Expected predicates to be a set, got {type(predicates)}"
