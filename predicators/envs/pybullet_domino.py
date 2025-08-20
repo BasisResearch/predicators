9e22a0c0444c1d7e51e5ac2cb9d20810e5a0856f
yichao-liang <ycliang6@gmail.com>
2025-08-20 19:38:36 +0100

    revert the goals because it'all blocks can't be all toppled due to the toppling delete law

--- predicators/envs/pybullet_domino.py
+++ predicators/envs/pybullet_domino.py
@@ -1518,9 +1518,9 @@ class PyBulletDominoEnv(PyBulletEnv):
                 # Find target dominoes (pink dominoes) and set them as goals
                 goal_atoms = set()
                 for domino_obj in init_state.get_objects(self._domino_type):
-                    goal_atoms.add(GroundAtom(self._Toppled, [domino_obj]))
-                    # if self._TargetDomino_holds(init_state, [domino_obj]):
-                    #     goal_atoms.add(GroundAtom(self._Toppled, [domino_obj]))
+                    # goal_atoms.add(GroundAtom(self._Toppled, [domino_obj]))
+                    if self._TargetDomino_holds(init_state, [domino_obj]):
+                        goal_atoms.add(GroundAtom(self._Toppled, [domino_obj]))
             else:
                 # Use regular targets
                 goal_atoms = {GroundAtom(self._Toppled, [self.targets[0]])}
