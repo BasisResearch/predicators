92436b7d6a3c9401cb4ab4100ea9c57202609f9b
yichao-liang <yichao-liang>
2025-08-22 09:25:08 -0400

    Add new script for creating bar plots of planning results and update output directory structure for classification plots

--- scripts/plotting/mara/create_bar_plots_solve.py
+++ scripts/plotting/mara/create_bar_plots_solve.py
@@ -25,73 +25,68 @@ X_LIM = (-5, 110)
 
 # Groups over which to take mean/std.
 GROUPS = [
-    "ENV",
-    "APPROACH",
-    # "EXCLUDED_PREDICATES",
-    "EXPERIMENT_ID",
-]
-
-env_names = [
-    "cover",
-    "blocks",
-    "coffee",
-    "balance",
-    "grow",
-    "circuit",
-    "float",
-    "domino",
-    "laser",
-    "ants",
-    "fan",
+    "ENV", "APPROACH", "EXCLUDED_PREDICATES", "EXPERIMENT_ID",
+    "ONLINE_LEARNING_CYCLE"
 ]
 
 # All column names and keys to load into the pandas tables.
 COLUMN_NAMES_AND_KEYS = [
     ("ENV", "env"),
     ("APPROACH", "approach"),
-    # ("EXCLUDED_PREDICATES", "excluded_predicates"),
+    ("EXCLUDED_PREDICATES", "excluded_predicates"),
     ("EXPERIMENT_ID", "experiment_id"),
     ("SEED", "seed"),
-    ("OVERALL_ACCURACY", "perc_overall_accuracy"),
-] + [(f"{env}_ACCURACY", f"perc_{env}_accuracy") for env in env_names]
-
-DERIVED_KEYS = [
-    ("perc_overall_accuracy", lambda r: 100 * r["avg_accuracy"]),
-    # In Python, when you create a lambda function inside a loop, it captures
-    # the variable by reference, not by value.
-    # When the lambda functions are actually called later, they all use the
-    # final value of env from the loop.
-    # use default arguments in the lambda to capture the current value:
-    *[(f"perc_{env}_accuracy", lambda r, env=env: 100 * r[f"{env}_accuracy"])
-      for env in env_names],
+    ("AVG_TEST_TIME", "avg_suc_time"),
+    ("AVG_NODES_CREATED", "avg_num_nodes_created"),
+    ("LEARNING_TIME", "learning_time"),
+    ("PERC_SOLVED", "perc_solved"),
+    ("ONLINE_LEARNING_CYCLE",
+     "cycle"),  # add to select model at specific cycle
+    ("AVG_NUM_FAILED_PLAN", "avg_num_skeletons_optimized"),
 ]
 
+DERIVED_KEYS = [("perc_solved",
+                 lambda r: 100 * r["num_solved"] / r["num_test_tasks"])]
+
 KEYS = [
-    "OVERALL_ACCURACY",
-    *[(f"{env}_ACCURACY") for env in env_names],
+    "PERC_SOLVED",
 ]
 
 # The keys of the dict are (df key, df value), and the dict values are
 # labels for the legend. The df key/value are used to select a subset from
 # the overall pandas dataframe.
 PLOT_GROUPS = [
-    ("", pd_create_equal_selector("ENV", "all_tasks")),
-    # ("Cover", pd_create_equal_selector("ENV", "pybullet_cover_typed_options")),
-    # ("Coffee", pd_create_equal_selector("ENV", "pybullet_coffee")),
-    # ("Cover Heavy", pd_create_equal_selector("ENV", "pybullet_cover_weighted")),
-    # ("Balance", pd_create_equal_selector("ENV", "pybullet_balance")),
+    ("Boil", pd_create_equal_selector("ENV", "pybullet_boil")),
+    ("Coffee", pd_create_equal_selector("ENV", "pybullet_coffee")),
+    ("Grow", pd_create_equal_selector("ENV", "pybullet_grow")),
+    ("Fan", pd_create_equal_selector("ENV", "pybullet_fan")),
+    ("Domino", pd_create_equal_selector("ENV", "pybullet_domino")),
 ]
 
 # See PLOT_GROUPS comment.
 BAR_GROUPS = [
+    ("Oracle",
+     lambda df: df["EXPERIMENT_ID"].apply(lambda v: "oracle" in v)),
+    # ("oracle invent",
+    #  lambda df: df["EXPERIMENT_ID"].apply(lambda v: "oracle_invention" in v)),
+    # ("oracle explore",
+    #  lambda df: df["EXPERIMENT_ID"].apply(lambda v: "oracle_explore" in v)),
     # ("Ours", lambda df: df["EXPERIMENT_ID"].apply(lambda v: "nsp-nl" in v)),
-    ("Human",
-     lambda df: df["EXPERIMENT_ID"].apply(lambda v: "human" in v)),
-    ("VLM", lambda df: df["EXPERIMENT_ID"].apply(lambda v: "vlm_clf" in v)),
-    ("DINO-dtw",
-     lambda df: df["EXPERIMENT_ID"].apply(lambda v: "dino_sim_dtw" in v)),
-    ("DINO-chf",
-     lambda df: df["EXPERIMENT_ID"].apply(lambda v: "dino_sim_chamfer" in v)),
+    # (
+    #     "MAPLE",
+    #     lambda df: (df["EXPERIMENT_ID"].apply(lambda v: "maple_q" in v)) &
+    #     # (df["ONLINE_LEARNING_CYCLE"].apply(lambda v: "19" == v))
+    #     (df["ONLINE_LEARNING_CYCLE"].apply(lambda v: "15" == v))  # blocks
+    # ),
+    # ("ViLa", lambda df: df["EXPERIMENT_ID"].apply(lambda v: "vlm_plan" in v)),
+    # ("Sym. pred.",
+    #  lambda df: df["EXPERIMENT_ID"].apply(lambda v: "interpret" in v)),
+    # # ("ablate select obj.",
+    # #  lambda df: df["EXPERIMENT_ID"].apply(lambda v: "no_acc_select" in v)),
+    # ("Ablate op.",
+    #  lambda df: df["EXPERIMENT_ID"].apply(lambda v: "no_new_op_learner" in v)),
+    # ("No invent",
+    #  lambda df: df["EXPERIMENT_ID"].apply(lambda v: "no_invent" in v)),
 ]
 
 #################### Should not need to change below here #####################
@@ -99,7 +94,7 @@ BAR_GROUPS = [
 
 def _main() -> None:
     outdir = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                          "results", "classification")
+                          "results", "planning")
     os.makedirs(outdir, exist_ok=True)
     matplotlib.rcParams.update({'font.size': FONT_SIZE})
 
@@ -130,11 +125,10 @@ def _main() -> None:
             ax.barh(plot_labels, plot_means, xerr=plot_stds, color='green')
             ax.set_xlim(X_LIM)
             ax.tick_params(axis='y', colors='black')
-            key_name = key.lower().replace("_", " ")
-            ax.set_title(key_name + plot_title)
+            ax.set_title(plot_title)
             plt.gca().invert_yaxis()
             plt.tight_layout()
-            filename = f"{plot_title}{key}.png"
+            filename = f"{plot_title}_{key}.png"
             filename = filename.replace(" ", "_").lower()
             outfile = os.path.join(outdir, filename)
             plt.savefig(outfile, dpi=DPI)
