a301d79feb43c7290c388cf57a5e5a5a894f1b60
yichao-liang <ycliang6@gmail.com>
2025-08-23 14:17:25 +0100

    Adjust jug_sample_x_max parameter in PyBulletBoilEnv for improved environment configuration

--- predicators/envs/pybullet_boil.py
+++ predicators/envs/pybullet_boil.py
@@ -85,7 +85,7 @@ class PyBulletBoilEnv(PyBulletEnv):
     jug_sample_y_margin_bot: ClassVar[float] = 0.4  # margin from y_lb
     jug_sample_y_margin_top: ClassVar[float] = 0.05  # margin from y_ub
     jug_sample_x_min: ClassVar[float] = x_mid
-    jug_sample_x_max: ClassVar[float] = x_mid + jug_sample_x_margin * 3.5
+    jug_sample_x_max: ClassVar[float] = x_mid + jug_sample_x_margin * 3
     jug_sample_y_min: ClassVar[float] = y_lb + jug_sample_y_margin_bot
     jug_sample_y_max: ClassVar[float] = y_ub - jug_sample_y_margin_top
 
