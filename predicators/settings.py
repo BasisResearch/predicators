02b1c4ce523b868696fd45bcf811c7bf39f0a8a2
yichao-liang <ycliang6@gmail.com>
2025-08-27 20:35:13 +0100

    Update domino_train_num_pos_y setting from 3 to 2 in GlobalSettings

--- predicators/settings.py
+++ predicators/settings.py
@@ -418,7 +418,7 @@ class GlobalSettings:
     domino_train_num_pivots = [0]
     domino_test_num_pivots = [0]
     domino_train_num_pos_x = 3
-    domino_train_num_pos_y = 3
+    domino_train_num_pos_y = 2
     domino_test_num_pos_x = 4  # 5 is too large for robot to reach sometimes
     domino_test_num_pos_y = 3
 
