e6729b10fe4b34d52f92afb949be859f7f1b36ce
yichao-liang <ycliang6@gmail.com>
2025-08-29 17:32:11 +0100

    Update OnlinePredicateInventionProcessPlanningApproach to use target_predicates and enhance causal_predicator_baselines.yaml with detailed coffee and grow configurations.

--- predicators/approaches/pp_online_predicate_invention_approach.py
+++ predicators/approaches/pp_online_predicate_invention_approach.py
@@ -54,7 +54,7 @@ class OnlinePredicateInventionProcessPlanningApproach(
                  option_model: Optional[_OptionModelBase] = None):
         # just used for oracle predicate proposal or learned predicate
         self._oracle_predicates = create_new_env(CFG.env,
-                                                 use_gui=False).predicates
+                                                use_gui=False).target_predicates
         self._candidate_predicates: Set[Predicate] = set()
         self._llm = utils.create_llm_by_name(CFG.llm_model_name)
         self._vlm = utils.create_vlm_by_name(CFG.llm_model_name)
